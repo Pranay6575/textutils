5d1b93274d16d74ef9d8c7d115615e659622c9a2
Pranay <pranaypatil6575@gmail.com>
2023-07-03 14:26:37 +0530

    Dark mode completed finally

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
+import React, { useState } from 'react';
+import TextForm from "./components/TextForm";
+
+
 function App() {
+
+ const [mode, setMode] = useState('light');
+
+ const toggleMode=()=>{
+  if(mode === 'light'){
+    setMode('dark')
+    document.body.style.backgroundColor = 'grey'
+  }
+  else{
+    setMode('light')
+    document.body.style.backgroundColor = 'white'
+  }
+ }
   return (
     <>
-      <Navbar title = "TextUtils"/>
+      <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/> 
 
       <div className="container"> 
-      {/* <TextForm  heading= 'Enter your Thoughts'/> */}
-      <About />
+      <TextForm  heading= 'Enter your Thoughts' mode={mode}/>
+      {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,22 +29,22 @@ export default function TextForm(props) {
     // setText("Update Text here") // Correct  way to change the state
     return (
         <>
-        <div className = 'container'>
+        <div className = 'container' style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value = {text} onChange={handleonChange} id="myText" rows="6"></textarea>
+                <textarea className="form-control" value = {text} onChange={handleonChange} style={{color: props.mode==='dark'?'white':'black'}} id="myText" rows="6"></textarea>
             </div>
             <button className="btn btn-secondary mx-2" onClick={handleClick}>Convert to Upper Case</button>
             <button className="btn btn-primary mx-2" onClick={handlelowClick}>Convert to Lower Case</button>
             <button className="btn btn-success mx-2" onClick={handlegreetClick}>Click for Greet</button>
         </div>
 
-        <div className="container mt-5" >
+        <div className="container mt-5" style={{color: props.mode==='dark'?'white':'black'}}>
         <h1>New Text Summary Here</h1>
         <h5>{text.split(" ").length} words and {text.length} characters</h5>
         <p>Words can read in {0.008 * text.split(" ").length} Minutes</p> 
         <h3>Preview:- </h3>
-        <p>{text}</p>  
+        <p>{text.length>0?text:'Write Something above to Preview here'}</p>  
         </div>
 
         </>
